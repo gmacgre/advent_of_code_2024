da1c3dd5da42e3ed6d56719bb7856a6434d6dc21
gmacgre <gmacgre@byu.edu>
2024-12-01 21:08:33 -0600

    Initial Commit Add driver.sh for building and testing Add base.py for templating Add .gitignore

---
+++ base.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin
+
+#Add pre-reading code here
+
+while True:
+    line = input.readline()
+    #interpret line
+    if line == "":
+        break
+
+    #Add Code to run each line of the input here
+
+
+#Add Post-Input code here
